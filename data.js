dae8b6a1100ee71e0f78bbc698d9c9b990974194
ddoyediran <ddoyediran@gmail.com>
2023-06-22 02:18:45 +0200

    Data partially loaded

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
-// import * as dfd from "danfojs-node";
+import dotenv from "dotenv";
+dotenv.config();
 import * as tf from "@tensorflow/tfjs";
 
 // Load data from the csv file where consume is the label
 const loadData = async () => {
   //   const dataUrl = "fuelcar.csv";
-  const blob = new Blob(["fuelcar.csv"], { type: "application/csv" });
-  const dataUrl = URL.createObjectURL(blob);
 
-  console.log(dataUrl);
+  const dataUrl = process.env.DATA_URL;
+
   const trainingData = tf.data.csv(dataUrl, {
     columnConfigs: {
       consume: {
