91c8defe53459dab3d869a034e719956f7d648d7
ddoyediran <ddoyediran@gmail.com>
2023-06-23 01:20:28 +0200

    Data successfully converted to tensor

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 import dotenv from "dotenv";
-
 dotenv.config();
+
+import * as tf from "@tensorflow/tfjs";
+import { splitData } from "./data.js";
+
+// Import the dataset
+
+// Convert the dataset to tensor data type format
+async function trainModel() {
+  //console.log(await splitData());
+  const data = await splitData();
+
+  // convert dataset to tensor
+  const tensorTrainDistance = tf.tensor1d(data.trainDistance);
+  const tensorTestDistance = tf.tensor1d(data.testDistance);
+  const tensorTrainTarget = tf.tensor1d(data.trainConsume);
+  const tensorTestTarget = tf.tensor1d(data.testConsume);
+
+  tensorTestDistance.print();
+}
+
+trainModel();
+
+// Develop the linear regression model
